74f04c9e51b19c84bba8713881522f42ec9c6379
Galking10 <krllglkn1807@gmail.com>
2022-09-03 12:52:38 +0200

    app ready

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -3,73 +3,71 @@ import { getConvert } from '../../api/api';
 import './styles.scss';
 
 export const Form = () => {
-  const [topSelect, setTopSelect] = useState("USD");
-  const [bottomSelect, setBottomSelect] = useState("UAH");
+  const [topSelect, setTopSelect] = useState('USD');
+  const [bottomSelect, setBottomSelect] = useState('UAH');
   const [topInput, setTopInput] = useState(1);
   const [bottomInput, setBottomInput] = useState(1);
 
-  const topCountCurrency = async (e) => {
+  const topCountCurrency = async e => {
     setTopSelect(e.target.value);
-    const data = await getConvert( bottomSelect, topInput, e.target.value);
+    const data = await getConvert(topInput, e.target.value, bottomSelect );
     setBottomInput(JSON.parse(data).result);
   };
 
-  const bottomCountCurrency = async (e) => {
+  const bottomCountCurrency = async e => {
     setBottomSelect(e.target.value);
-    const data = await getConvert(topSelect, bottomInput, e.target.value);
+    const data = await getConvert(bottomInput, topSelect,  e.target.value);
     setTopInput(JSON.parse(data).result);
   };
 
-  const TopCountInput = async (e) => {
+  const TopCountInput = async e => {
     setTopInput(e.target.value);
-    const data = await getConvert(e.target.value, topSelect, bottomSelect );
+    const data = await getConvert(e.target.value, topSelect, bottomSelect);
     setBottomInput(JSON.parse(data).result);
   };
 
-  const bottomCountInInput = async (e) => {
+  const bottomCountInInput = async e => {
     setBottomInput(e.target.value);
-    const data = await getConvert(e.target.value, bottomSelect, topSelect );
+    const data = await getConvert(e.target.value, bottomSelect, topSelect);
     setTopInput(JSON.parse(data).result);
   };
-
 
   return (
-    <section className="converter">
+    <section>
       <div className="wrapper">
-        <header>Currency Converter</header>
-        <form action="#">
+        <h2>Currency Converter</h2>
+        <form>
           <div className="drop-list">
             <div className="amount">
-              <div className="inputs">
+              <div>
                 <p>Enter Amount</p>
-                <input className="top-input" type="text" value={topInput} onChange={TopCountInput} />
+                <input type="text" value={topInput} onChange={TopCountInput} />
+              </div>
+              <div>
                 <p>Enter Amount</p>
-                <input type="text"  value={bottomInput} onChange={bottomCountInInput}/>
+                <input
+                  type="text"
+                  value={bottomInput}
+                  onChange={bottomCountInInput}
+                />
               </div>
             </div>
             <div>
               <div className="from">
                 <p>From</p>
-                <div className="select-box">
-                  <select value={topSelect} onChange={topCountCurrency}>
-                    <option value="USD">USD</option>
-                    <option value="EUR">EUR</option>
-                    <option value="UAH">UAH</option>
-                  </select>
-                </div>
-              </div>
-              <div className="icon">
-                <i className="fas fa-exchange-alt"></i>
+                <select value={topSelect} onChange={topCountCurrency}>
+                  <option value="USD">USD</option>
+                  <option value="EUR">EUR</option>
+                  <option value="UAH">UAH</option>
+                </select>
               </div>
               <div className="to">
                 <p>To</p>
-                <div className="select-box">
-                  <select value={bottomSelect} onChange={bottomCountCurrency}>
-                    <option value="USD">USD</option>
-                    <option value="EUR">EUR</option>
-                    <option value="UAH">UAH</option>
-                  </select>
-                </div>
+                <select value={bottomSelect} onChange={bottomCountCurrency}>
+                  <option value="USD">USD</option>
+                  <option value="EUR">EUR</option>
+                  <option value="UAH">UAH</option>
+                </select>
               </div>
             </div>
           </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,28 @@
-import { useEffect, useState } from 'react'
-import { getConvert } from '../../api/api'
-import './styles.scss'
+import { useEffect, useState } from 'react';
+import { getConvert } from '../../api/api';
+import './styles.scss';
 
-export const Header = ()=>{
-    const [dollar, setDollar] = useState(0);
-    const [euro, setEuro] = useState(0);
+export const Header = () => {
+  const [dollar, setDollar] = useState(0);
+  const [euro, setEuro] = useState(0);
 
-
-
-    useEffect(()=>{
-        getConvert('1', 'USD', 'UAH' ).then((data) => setDollar(JSON.parse(data).result))
-        getConvert('1', 'EUR', 'UAH' ).then((data) => setEuro(JSON.parse(data).result))
-    }, [])
-    return(
-        <header className='header'>
-            <h1 className="title">Currency Converter</h1>
-            <ul>
-                <li>1 USD = {dollar.toFixed(2)} UAH</li>
-                <li>1 EUR = {euro.toFixed(2)} UAH</li>
-            </ul>
-        </header>
-    )
-}
+  useEffect(() => {
+    getConvert('1', 'USD', 'UAH').then(data =>
+      setDollar(JSON.parse(data).result),
+    );
+    getConvert('1', 'EUR', 'UAH').then(data =>
+      setEuro(JSON.parse(data).result),
+    );
+  }, []);
+  return (
+    <header >
+        <div className='header-container'>
+      <h1 className="title">Exchange Rate</h1>
+      <ul className='rate'>
+        <li>1 USD = {dollar.toFixed(2)} UAH</li>
+        <li>1 EUR = {euro.toFixed(2)} UAH</li>
+      </ul>
+      </div>
+    </header>
+  );
+};
